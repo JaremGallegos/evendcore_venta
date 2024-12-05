c6d621d14d4e67586a270d0aa4b9772296d91400
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2024-12-05 15:46:31 -0500

    evendcore_venta

---
+++ WebAplicacionVentas/Controllers/LandingController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebAplicacionVentas.Models;
+
+namespace WebAplicacionVentas.Controllers;
+
+public class LandingController : Controller
+{
+    private readonly ILogger<LandingController> _logger;
+
+    public LandingController(ILogger<LandingController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ WebAplicacionVentas/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebAplicacionVentas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
